a25336c20a9769c3531fdc6a0ed581b886711685
AndreiaSandovalGomes <gomesandreia27@outlook.com>
2018-06-06 15:57:08 -0300

    funcao criptografia - verificar loop

--- Cifra/app.js
+++ Cifra/app.js
@@ -1,16 +1,27 @@
 
-var promptWord = prompt('Digite sua palavra');
-var newStrings = promptWord.split('');
-console.log(newStrings)
-//console.log(promptWord)
-function cipher(newStrings) {
-  var newArray = '';
-  for (i= 0; i < newStrings.length; i++ );
-  {var res = newStrings.chacodeAt(i);
-    if (res >= 65 && res <=  90) {return newArray += String.fromCharCode((res - 65 + 7) % 26 + 65);  // Uppercase
-    }else if (res >= 97 && res <= 122) {return newArray += String.fromCharCode((res - 97 + 7) % 26 + 97);// Lowercase
-    }
+
+
+function cipher(){
+  var string = prompt('Digite sua palavra');
+  // var string = promptWord.split('');
+  var newString = '';
+  var res;
+  for (i= 0; i < string.length; i++){
+    res = string.charCodeAt(i);
+    console.log(res);
+//     res =
+//
+//
+//     if (res >= 65 && res <=  90) {newStrings  = String.fromCharCode((res - 65 + 7) % 26 + 65);  // Uppercase
+//     }else if (res >= 97 && res <= 122) { newStrings = String.fromCharCode((res - 97 + 7) % 26 + 97);// Lowercase
+//     }
   }
-return newArray;
+// return newString;
 }
-console.log(cipher(newStrings))
+cipher();
+// function bicho(){
+//   console.log('gato');
+//   var hamister = 2;
+//   return hamister
+// }
+// bicho();
